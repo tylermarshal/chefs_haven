d18afb2b1ef6b0f786c6ad86fdcb60312e868b27
TMad <madsen.tyler@gmail.com>
2018-04-01 11:24:30 -0600

    Convert calories to  integer

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,7 +6,7 @@ class Recipe
     @name = recipe[:recipe][:label]
     @url = recipe[:recipe][:url]
     @yield = recipe[:recipe][:yield]
-    @calories = recipe[:recipe][:calories]
+    @calories = recipe[:recipe][:calories].to_i
     @ingredients = recipe[:recipe][:ingredientLines].map {|ingredient| Ingredient.new(ingredient)}
   end
 
