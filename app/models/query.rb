a2ec88b097e67719c56ddab9939afc88fc6f9a11
TMad <madsen.tyler@gmail.com>
2018-04-01 10:37:50 -0600

    Add Query model spec for sort method

--- app/models/query.rb
+++ app/models/query.rb
@@ -1,2 +1,9 @@
 class Query < ApplicationRecord
+
+  has_many :search_terms
+
+  def self.sort(search_term, direction)
+    order("#{search_term} #{direction}")
+  end
+
 end
--- spec/models/query_spec.rb
+++ spec/models/query_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe Query, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Query do
+  context "class methods" do
+    context ".sort" do
+      it "sorts query objects by attribute" do
+        lasagna = create(:query, name: "lasagna", updated_at: Time.now)
+        pasta = create(:query, name: "pasta", updated_at: Time.now - 500, search_terms_count: 7)
+        chicken_alfredo = create(:query, updated_at: Time.now - 1000, search_terms_count: 2)
+
+        expect(Query.sort("name", "asc")).to eq([chicken_alfredo, lasagna, pasta])
+        expect(Query.sort("name", "desc")).to eq([pasta, lasagna, chicken_alfredo])
+        expect(Query.sort("updated_at", "desc")).to eq([lasagna, pasta, chicken_alfredo])
+        expect(Query.sort("updated_at", "asc")).to eq([chicken_alfredo, pasta, lasagna])
+        expect(Query.sort("search_terms_count", "desc")).to eq([pasta, chicken_alfredo, lasagna])
+        expect(Query.sort("search_terms_count", "asc")).to eq([lasagna, chicken_alfredo, pasta])
+      end
+    end
+  end
 end
